b5c99a3e2d7118314a4e45668f308752bc247f2f
mbecker <mats.becker@gmail.com>
2022-04-20 20:43:50 +0200

    Make migrat files executable

--- migrate/01-download.sh
+++ migrate/01-download.sh
@@ -1,3 +1,2 @@
-curl -L https://github.com/golang-migrate/migrate/releases/download/v4.15.1/migrate.linux-amd
-64.tar.gz | tar xvz
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.15.1/migrate.linux-amd64.tar.gz | tar xvz
 sudo mv migrate /usr/bin/
